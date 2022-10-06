cd0b6166ffff49d228c38ee0aeacdc743cc9880b
FL3TCH0R <114429899+FL3TCH0R@users.noreply.github.com>
2022-10-06 13:49:11 -0400

    Add files via upload

---
+++ Question1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.question1;
+
+/**
+ *
+ * @author fletc
+ */
+public class Question1 {
+
+    public static void main(String[] args) {
+        
+       int a= 21;
+       int b= 7;
+       
+       System.out.println(a+b);
+       System.out.println(a-b);
+       System.out.println(a*b);
+       System.out.println(a/b);
+       System.out.println(a%b);
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
---
+++ Question2.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.question2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author fletc
+ */
+public class Question2 {
+
+    public static void main(String[] args) {
+        
+     Scanner input=new Scanner(System.in);
+         int r=9;
+        final double PI=3.14
+     
+     if(r>0)
+     {
+     System.out.println("The radius is greater than or equal to 0.");
+     }
+     
+     else
+     {
+     System.out.println("Negative input");
+     
+     }
+     
+     A=(2*PI*r*2)
+     
+     
+     System.out.println("Enter the area"+A);
+     
+     
+     
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }      
+}
+    
+
+
